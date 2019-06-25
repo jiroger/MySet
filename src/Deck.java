6a9462a191cd891483c083b1dc3f7c4d2b2a194b
roger ji <roger.ji.32021@gmail.com>
2019-06-25 11:41:40 -0700

    finished deck class

--- src/Deck.java
+++ src/Deck.java
@@ -1,13 +1,36 @@
+import java.util.ArrayList;
+
+import processing.core.PApplet;
 
 public class Deck {
+	PApplet parent;
+	private ArrayList<Card> cards = new ArrayList<Card>();
 
-	public Deck(SET_Final set_Final) {
-		// TODO Auto-generated constructor stub
+	public Deck(PApplet p) {
+		parent = p;
+		initDeck();
 	}
 
-	public Object deal() {
-		// TODO Auto-generated method stub
-		return null;
+	public void initDeck() {
+		for (int col = 0; col < SET_Final.SHEET_LENGTH; col++) {
+			for (int row = 0; row < SET_Final.SHEET_LENGTH; row++) {
+				cards.add(new Card(col, row, parent));
+			}
+		}
 	}
 
+	public Card getCard(int n) {
+		return cards.get(n);
+	}
+
+	public Card deal() {
+		if (cards.size() == 0)
+			return null;
+
+		return cards.remove((int) (Math.random() * cards.size()));
+	}
+
+	public int size() {
+		return cards.size();
+	}
 }
